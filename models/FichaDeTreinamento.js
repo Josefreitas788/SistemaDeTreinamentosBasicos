3a0fe1e141fc6f5ed3de19dd915c3cc24be54aa9
José Antônio <57533270+Josefreitas788@users.noreply.github.com>
2022-07-11 18:56:42 +0000

    Atualizacao no model fichaDeTreinamento

--- models/FichaDeTreinamento.js
+++ models/FichaDeTreinamento.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 require('./Exercicio');
+require('./FichaDeTreinamento');
 
 
 const FichaDeTreinamento = mongoose.model("FichaDeTreinamento", new mongoose.Schema({
-    id_user: String,
     nivel: String, //nivel da pessoa
 		frequencia: String, //quant de dias por semana
 		avaliacaoPosTreino: String,
@@ -11,6 +11,10 @@ const FichaDeTreinamento = mongoose.model("FichaDeTreinamento", new mongoose.Sch
     exercicio: [{
       type: mongoose.Schema.Types.ObjectId,
       ref: "Exercicio"
+    }],
+    usuario : [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Usuario"
     }]
 }))
 
